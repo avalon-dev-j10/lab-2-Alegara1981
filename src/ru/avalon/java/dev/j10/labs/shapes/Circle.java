a5478666f9fbfa650c0eacaaa742e852a6c923e2
Alegara1981 <48486319+Alegara1981@users.noreply.github.com>
2019-03-29 18:40:07 +0300

    Update Circle.java

--- src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
@@ -1,14 +1,14 @@
 package ru.avalon.java.dev.j10.labs.shapes;
 
-public class Circle implements Ellipse {
-   private final float r;
-   private final float x;
-   private final float y;
+public class Circle implements Ellipse {  // Обьявлен класс Круг наследующий интерфейс элипс
+   private final float r;  //Объявлены поля класса радиус
+   private final float x;   // координата икс
+   private final float y;     // координата игрек
    
   
 
-    public Circle() {
-        r = (float) (100 * Math.random());
+    public Circle() { // инициализация полей класса
+        r = (float) (100 * Math.random()); 
         x = (float) (100 * Math.random());
         y = (float) (100 * Math.random());
           
@@ -20,28 +20,28 @@ public class Circle implements Ellipse {
     */
 
     @Override
-    public float getLength() {
+    public float getLength() { // метод полученя периметра круга
         return (float) (2 * pi * r);
     }
 
     @Override
-    public float getArea() {
+    public float getArea() {   // метод получения площади окружности
        return (float) (pi * r * r);
     }
 
     @Override
-    public int getRotation() {
+    public int getRotation() { // метод получения угла наклона окружности
         return 0;
     }
 
     @Override
-    public float getX() {
+    public float getX() {  // метод получения координаты икс окружности
         return x; 
     }
 
     @Override
-    public float getY() {
-        return y; //To change body of generated methods, choose Tools | Templates.
+    public float getY() {  //метод получения координаты игрек
+        return y;
     }
     
     
@@ -52,7 +52,7 @@ public class Circle implements Ellipse {
      */
 
     @Override
-    public String toString() {
+    public String toString() { // метод преобразования числа в строку
         return "Circle";
     }
 }
